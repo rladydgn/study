0a95be8bf172bd9dcbd6eceff76d90f574fa658b
Yonghoo Kim <dhdgn@naver.com>
2022-04-08 14:59:19 +0900

    'diary'

---
+++ diary/app/src/main/java/com/example/myapplication/DBHelper.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE if not exists diary ("
+                + "_id integer primary key autoincrement,"
+                + "subject text not null,"
+                + "content text not null,"
+                + "date text not null);";
+
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql = "DROP TABLE if exists diary";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+}
