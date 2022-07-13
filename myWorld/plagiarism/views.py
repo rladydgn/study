9165fdc4577b3d515c3cc83979d9950f8b9be1b8
rladydgn <39542757+rladydgn@users.noreply.github.com>
2022-07-13 23:15:42 +0900

    Add files via upload

--- myWorld/plagiarism/views.py
+++ myWorld/plagiarism/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
+import random
+
 
 # def index(request):
 #    return HttpResponse("hello world!")
@@ -9,3 +11,8 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "plagiarism/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['number'] = random.randrange(1, 100)
+        return context
