e878e1d180087ac8ccac3d7b8268f233a51efc00
rladydgn <39542757+rladydgn@users.noreply.github.com>
2022-11-19 13:43:35 +0900

    square detection with JNI, c++ openCV, android camera2 api

---
+++ OpenCVTest2/opencvtest2.md
@@ -0,0 +1,5 @@
+OpenCV Project
+
+camera2 api를 이용함.
+camera project의 기능들을 넣고, 추가로 카메라가 보는 이미지를 opencv를 이용하여
+흑백으로 바꿔준 뒤 imageView에 실시간으로 보여주는 기능을 추가함.
---
+++ OpenCVTest2/sdk/build/intermediates/cxx/Debug/6v4q2y39/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\SnapTagRND\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\SnapTagRND\\AndroidStudioProjects\\OpenCVTest2\\sdk\\.cxx\\Debug\\6v4q2y39\\arm64-v8a" ^
+  opencv_jni_shared
