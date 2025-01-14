e286d3b1f767608ea1762642545d93c672051faa
rladydgn <dhdgn@naver.com>
2025-01-14 16:26:55 +0900

    환율 정보 가져오는 로직 클래스 분리

---
+++ src/main/java/tobyspring/hellospring/WebApiExRatePaymentService.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WebApiExRatePaymentService extends PaymentService {
+	@Override
+	BigDecimal getExRate(String currency) throws IOException {
+		URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String response = br.lines().collect(Collectors.joining());
+		br.close();
+
+		ObjectMapper mapper = new ObjectMapper();
+		ExRateDate data = mapper.readValue(response, ExRateDate.class);
+		BigDecimal exRate = data.rates().get("KRW");
+		System.out.println(exRate);
+		return exRate;
+	}
+}
