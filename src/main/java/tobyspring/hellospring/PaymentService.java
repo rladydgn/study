91e87c767d051063c58ab3209c1f022fff065937
rladydgn <dhdgn@naver.com>
2025-01-14 18:35:08 +0900

    WebApiExRateProvider 클래스 인스턴스로 이동

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -5,8 +5,13 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
+	private WebApiExRateProvider exRateProvider = new WebApiExRateProvider();
+
+	public PaymentService() {
+		this.exRateProvider = new WebApiExRateProvider();
+	}
+
 	public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-		WebApiExRateProvider exRateProvider = new WebApiExRateProvider();
 		BigDecimal exRate = exRateProvider.getWebExRate(currency);
 		BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
 		LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
