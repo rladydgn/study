1feb78bcb5955dfc30a48c8f46f818d89befc608
rladydgn <dhdgn@naver.com>
2025-01-15 12:15:34 +0900

    누락된 어노테이션 추가

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,6 +1,5 @@
 package tobyspring.hellospring;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
