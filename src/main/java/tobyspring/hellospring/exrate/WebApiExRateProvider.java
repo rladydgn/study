db17323282706b9e9e7db049b48ed04e9c48a311
rladydgn <dhdgn@naver.com>
2025-01-21 20:12:07 +0900

    템플릿 분리

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,49 +1,19 @@
 package tobyspring.hellospring.exrate;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import tobyspring.hellospring.api.ApiExecutor;
+import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
-import tobyspring.hellospring.api.ExRateExtractor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
 	@Override
 	public BigDecimal getExRate(String currency) {
+		ApiTemplate apiTemplate = new ApiTemplate();
 		String url = "https://open.er-api.com/v6/latest/" + currency;
 
 		// 콜백
-		// runApiForExRate(url, uri -> {});
-		return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
-	}
-
-	// 템플릿
-	private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-		URI uri;
-		try {
-			uri = new URI(url);
-		} catch (URISyntaxException e) {
-			throw new RuntimeException(e);
-		}
-
-		String response;
-		try {
-			response = apiExecutor.execute(uri);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		try {
-			return exRateExtractor.extract(response);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
+		return apiTemplate.getExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
 	}
 }
