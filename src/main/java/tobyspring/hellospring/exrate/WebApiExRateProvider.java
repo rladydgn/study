9f9152749c9ce603fa98e96a52abca3d40b9d612
rladydgn <dhdgn@naver.com>
2025-01-21 20:20:47 +0900

    api 호출하는 java api 추가

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -4,16 +4,19 @@ import java.math.BigDecimal;
 
 import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
-import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.api.HttpClientApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
+	ApiTemplate apiTemplate = new ApiTemplate();
+
 	@Override
 	public BigDecimal getExRate(String currency) {
-		ApiTemplate apiTemplate = new ApiTemplate();
+
 		String url = "https://open.er-api.com/v6/latest/" + currency;
 
 		// 콜백
-		return apiTemplate.getExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+		// return apiTemplate.getExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+		return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
 	}
 }
