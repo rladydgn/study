6233ea6f6faf0dea03261c6bdee1e7d86cf5a65d
rladydgn <dhdgn@naver.com>
2025-01-21 17:43:28 +0900

    자바버전 21 상승, exception throw 리팩토링

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -5,27 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import tobyspring.hellospring.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
 	@Override
-	public BigDecimal getExRate(String currency) throws IOException {
-		URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String response = br.lines().collect(Collectors.joining());
-		br.close();
+	public BigDecimal getExRate(String currency) {
+		String url = "https://open.er-api.com/v6/latest/" + currency;
+		URI uri;
+		try {
+			uri = new URI(url);
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
 
-		ObjectMapper mapper = new ObjectMapper();
-		ExRateDate data = mapper.readValue(response, ExRateDate.class);
+		String response;
+		try {
+			HttpURLConnection connection = (HttpURLConnection)uri.toURL().openConnection();
+			BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			response = br.lines().collect(Collectors.joining());
+			br.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-		System.out.println("API ExRate: " + data.rates().get("KRW"));
-
-		return data.rates().get("KRW");
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			ExRateDate data = mapper.readValue(response, ExRateDate.class);
+			System.out.println("API ExRate: " + data.rates().get("KRW"));
+			return data.rates().get("KRW");
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
