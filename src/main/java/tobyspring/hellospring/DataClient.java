c7851fb1f20834f5daa4d34e5dd233d45d4140d3
rladydgn <dhdgn@naver.com>
2025-01-23 17:37:42 +0900

    템플릿 사용

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
@@ -12,12 +14,18 @@ public class DataClient {
 	public static void main(String[] args) {
 		BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
 		OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+		JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-		Order order = new Order("100", BigDecimal.TEN);
-		repository.save(order);
-		System.out.println(order);
+		new TransactionTemplate(transactionManager).execute(status -> {
+			Order order = new Order("100", BigDecimal.TEN);
+			repository.save(order);
+			System.out.println(order);
 
-		Order order2 = new Order("100", BigDecimal.TEN);
-		repository.save(order2);
+			Order order2 = new Order("100", BigDecimal.TEN);
+			repository.save(order2);
+
+			return null;
+		});
+
 	}
 }
--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -2,11 +2,14 @@ package tobyspring.hellospring;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
@@ -37,7 +40,17 @@ public class DataConfig {
 	}
 
 	@Bean
+	public BeanPostProcessor persistenceAnnotationBeanPostProcessor() {
+		return new PersistenceAnnotationBeanPostProcessor();
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+
+	@Bean
 	public OrderRepository orderRepository(EntityManagerFactory emf) {
-		return new OrderRepository(emf);
+		return new OrderRepository();
 	}
 }
