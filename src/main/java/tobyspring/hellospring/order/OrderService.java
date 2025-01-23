0a7da2a54d1a12f2b1a1ebb4b8cf5e24f58f7f1a
rladydgn <dhdgn@naver.com>
2025-01-24 00:34:05 +0900

    transaction 추상화

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import tobyspring.hellospring.data.OrderRepository;
@@ -11,9 +12,9 @@ import tobyspring.hellospring.data.OrderRepository;
 @Service
 public class OrderService {
 	private final OrderRepository orderRepository;
-	private final JpaTransactionManager transactionManager;
+	private final PlatformTransactionManager transactionManager;
 
-	public OrderService(OrderRepository orderRepository, JpaTransactionManager transactionManager) {
+	public OrderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
 		this.orderRepository = orderRepository;
 		this.transactionManager = transactionManager;
 	}
