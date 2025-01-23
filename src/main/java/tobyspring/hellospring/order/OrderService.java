dc4ed5dc9de5b011465e328eb504e8691cb12c5c
rladydgn <dhdgn@naver.com>
2025-01-24 01:45:17 +0900

    order 목록 생성

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -1,10 +1,10 @@
 package tobyspring.hellospring.order;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 public class OrderService {
@@ -19,9 +19,11 @@ public class OrderService {
 	public Order createOrder(String no, BigDecimal total) {
 		Order order = new Order(no, total);
 
-		return new TransactionTemplate(transactionManager).execute(status -> {
-			this.orderRepository.save(order);
-			return order;
-		});
+		this.orderRepository.save(order);
+		return order;
+	}
+
+	public List<Order> createOrders(List<OrderReq> reqs) {
+		return reqs.stream().map(req -> createOrder(req.orderNumber(), req.total())).toList();
 	}
 }
--- src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -1,6 +1,7 @@
 package tobyspring.hellospring.order;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,18 @@ public class OrderServiceSpringTest {
 		Order order = orderService.createOrder("0100", BigDecimal.ONE);
 
 		Assertions.assertThat(order.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	void createOrders() {
+		List<OrderReq> orderReqs = List.of(
+			new OrderReq("0200", BigDecimal.ONE),
+			new OrderReq("0300", BigDecimal.ONE)
+		);
+
+		List<Order> orders = orderService.createOrders(orderReqs);
+
+		Assertions.assertThat(orders).hasSize(2);
+		orders.forEach(order -> Assertions.assertThat(order.getId()).isGreaterThan(0));
 	}
 }
