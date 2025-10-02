495b0eed23eb715cfd956745c1fda4a337c1f51f
rladydgn <dhdgn@naver.com>
2025-10-02 10:54:10 +0900

    9장 서버 컴포넌트

--- nextjs-soaple/src/app/feed/(..)posts/[postId]/page.tsx
+++ nextjs-soaple/src/app/feed/(..)posts/[postId]/page.tsx
@@ -1,4 +1,6 @@
 import PostFragment from "@/components/PostFragment";
+import samplePosts from "@/lib/constants/sample-posts.json"
+import {notFound} from "next/navigation";
 
 interface PageProps {
   params: Promise<{ postId: string }>;
@@ -6,13 +8,19 @@ interface PageProps {
 
 // /feed 에서 Link 컴포넌트 사용해서 라우팅시 intercept 되어 보여지는 페이지
 export default async function PostItemPage(props: PageProps) {
-  const { params } = props;
-  const { postId } = await params;
+  const {params} = props;
+  const {postId} = await params;
+
+  const post = samplePosts.find((post) => post.id === postId);
+
+  if (!post) {
+    notFound();
+  }
 
   return (
     <div className='box page'>
       <p>{`intercepted PostItemPage ${postId}`}</p>
-      <PostFragment postId={postId} />
+      <PostFragment post={post}/>
     </div>
   );
 }
--- nextjs-soaple/src/app/posts/[postId]/page.tsx
+++ nextjs-soaple/src/app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
 import {Metadata} from "next";
 import PostFragment from "@/components/PostFragment";
+import samplePosts from "@/lib/constants/sample-posts.json"
+import {notFound} from "next/navigation";
 
 interface PageProps {
   params: Promise<{ postId: string }>;
@@ -22,10 +24,16 @@ export default async function PostItemPage(props: PageProps) {
   const {params} = props;
   const {postId} = await params;
 
+  const post = samplePosts.find(post => post.id === postId);
+
+  if (!post) {
+    notFound()
+  }
+
   return (
     <div className='box page'>
       <p>{`PostItemPage ${postId}`}</p>
-      <PostFragment postId={postId}/>
+      <PostFragment post={post}/>
     </div>
   )
 }
